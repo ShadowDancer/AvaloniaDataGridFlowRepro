c0da83af622b3462df4be1c0e2978f04e85846d2
Przemysław Onak <przemekonak@gmial.com>
2020-06-30 23:20:29 +0200

    Add project files.

---
+++ AvaloniaFlowRepro/CustomColumn.cs
@@ -0,0 +1,27 @@
+﻿using Avalonia.Controls;
+using Avalonia.Controls.Utils;
+using Avalonia.Interactivity;
+
+namespace AvaloniaFlowRepro
+{
+    public class CustomColumn : DataGridBoundColumn
+    {
+        protected override IControl GenerateEditingElementDirect(DataGridCell cell, object dataItem)
+        {
+
+            ListBox lb = new ListBox();
+            lb.Height = 100;
+            return lb;
+        }
+
+        protected override IControl GenerateElement(DataGridCell cell, object dataItem)
+        {
+            return new TextBlock();
+        }
+
+        protected override object PrepareCellForEdit(IControl editingElement, RoutedEventArgs editingEventArgs)
+        {
+            return null;
+        }
+    }
+}
---
+++ AvaloniaFlowRepro/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,34 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace AvaloniaFlowRepro.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private List<Obj> items;
+
+        public MainWindowViewModel()
+        {
+            MakeItems(30);
+        }
+
+        private void MakeItems(int count)
+        {
+            Items = Enumerable.Range(0, count).Select(n => new Obj() { Name = n.ToString() }).ToList();
+        }
+
+        public List<Obj> Items { get => items; set => this.RaiseAndSetIfChanged(ref items, value); }
+
+    }
+    public class Obj
+    {
+        public string Name { get; set; }
+
+
+        public string NullProp { get; set; }
+    }
+}
